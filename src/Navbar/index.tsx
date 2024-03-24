902c32484eb24fb3fb9cea8570b86e9f546cafd8
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2024-03-24 15:44:24 -0700

    basic ui working

---
+++ src/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div className='navbar-container'>
+            <img className='ultron-logo' src={"https://cylab-temp-testing-bucket.s3.amazonaws.com/images/ultron-logo.svg"}></img>
+            <img className="cart-logo" src={"https://cylab-temp-testing-bucket.s3.amazonaws.com/images/cart-logo.png"}></img>
+        </div>
+    )
+}
+
+export default Navbar
